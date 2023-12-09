c4e4bcde2f8e97b4503d35703544779e17ba01c5
khaled gamal <khaledgamal1345@gmail.com>
2023-12-09 16:59:51 +0200

    installed rest and added its settings, added urls routes for api endpoints

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,10 +37,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #3rd party apps
+    'rest_framework',
     #local
     'accounts.apps.AccountsConfig',
     'posts.apps.PostsConfig',
 ]
+REST_FRAMEWORK = { 
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.AllowAny",
+],
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
