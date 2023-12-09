20459c6c1aaa5f73acddaa979a6f3e876bb44412
khaled gamal <khaledgamal1345@gmail.com>
2023-12-09 18:07:52 +0200

    sets cors headers and csrf trusted origins

--- django_project/settings.py
+++ django_project/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # 3rd-party apps
     "rest_framework",
+    'corsheaders',
+    # local
     'accounts.apps.AccountsConfig',
     'posts.apps.PostsConfig',
 ]
@@ -52,12 +54,18 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:3000',
+    'http://localhost:8000',
+)
+CSRF_TRUSTED_ORIGINS = ['http://localhost:3000']
 
 ROOT_URLCONF = 'django_project.urls'
 
