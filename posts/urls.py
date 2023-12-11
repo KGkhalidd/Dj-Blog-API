4e77458c47e243636dcf69b52c175d1f62d07354
khaled gamal <khaledgamal1345@gmail.com>
2023-12-12 01:41:16 +0200

    update our urls with routers for viewsets

--- posts/urls.py
+++ posts/urls.py
@@ -1,9 +1,15 @@
-from django.urls import path
-from .views import PostList, PostDetail, UserList, UserDetail
+from rest_framework.routers import SimpleRouter
+from .views import UserViewSet, PostViewSet
 
-urlpatterns = [
-    path("users/", UserList.as_view() ),
-    path("users/<int:pk>/", UserDetail.as_view() ),
-    path("<int:pk>/", PostDetail.as_view(), name="post_detail"),
-    path("", PostList.as_view(), name="post_list"),
-]
+router = SimpleRouter()
+router.register('users', UserViewSet, basename='users')
+router.register('', PostViewSet, basename='posts')
+
+urlpatterns = router.urls
+
+# urlpatterns = [
+#     path("users/", UserList.as_view() ),
+#     path("users/<int:pk>/", UserDetail.as_view() ),
+#     path("<int:pk>/", PostDetail.as_view(), name="post_detail"),
+#     path("", PostList.as_view(), name="post_list"),
+# ]
