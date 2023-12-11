1771bd29f5980262dcf0be5f15c3005274b7570a
khaled gamal <khaledgamal1345@gmail.com>
2023-12-11 23:34:09 +0200

    added serializer for our CustomUser model

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from .models import Post
 
@@ -11,3 +12,8 @@ class PostSerializer(serializers.ModelSerializer):
             'body',
             'created_at',
         )
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ('id', 'username')
