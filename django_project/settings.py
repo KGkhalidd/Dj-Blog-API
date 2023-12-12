668e7f444be3648fa04ee4140a4debf78d064e68
khaled gamal <khaledgamal1345@gmail.com>
2023-12-12 18:52:49 +0200

    generate the schema as a standalone file & dynamicaly with spectular third pary apps , made a doc with Redoc & SwaggerUI

--- django_project/settings.py
+++ django_project/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'dj_rest_auth',
     'dj_rest_auth.registration',
+    'drf_spectacular',
     # local
     'accounts.apps.AccountsConfig',
     'posts.apps.PostsConfig',
@@ -60,6 +61,12 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ],
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Blog API Project",
+    "DESCRIPTION": "A sample blog to learn about DRF",
+    "VERSION": "1.0.0",
 }
 
 MIDDLEWARE = [
